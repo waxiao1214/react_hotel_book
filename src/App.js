8d4108c348435206ef1c73f5a274346ec5b63f84
highqcoder <wxiao1214@gmail.com>
2019-05-10 22:02:23 +0200

    Create file structure & add data file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
+import Home from './components/Home';
+import RoomsList from './components/RoomsList';
+import Room from './components/Room';
+import Error from './components/Error';
 
 function App() {
   return (
     <React.Fragment>
       <h3>hello from App</h3>
+      <Home />
+      <RoomsList />
+      <Room />
+      <Error />
     </React.Fragment>
   );
 }
