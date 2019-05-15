0179015dc631c283bfbcbc323e24dbe3e1072cdd
highqcoder <wxiao1214@gmail.com>
2019-05-15 06:43:53 +0200

    Move data file into src dir

--- src/data.js
+++ src/data.js
@@ -1,19 +1,19 @@
-import room1 from "./images/details-1.jpeg";
-import room2 from "./images/details-2.jpeg";
-import room3 from "./images/details-3.jpeg";
-import room4 from "./images/details-4.jpeg";
-import img1 from "./images/room-1.jpeg";
-import img2 from "./images/room-2.jpeg";
-import img3 from "./images/room-3.jpeg";
-import img4 from "./images/room-4.jpeg";
-import img5 from "./images/room-5.jpeg";
-import img6 from "./images/room-6.jpeg";
-import img7 from "./images/room-7.jpeg";
-import img8 from "./images/room-8.jpeg";
-import img9 from "./images/room-9.jpeg";
-import img10 from "./images/room-10.jpeg";
-import img11 from "./images/room-11.jpeg";
-import img12 from "./images/room-12.jpeg";
+import room1 from "./img/details-1.jpeg";
+import room2 from "./img/details-2.jpeg";
+import room3 from "./img/details-3.jpeg";
+import room4 from "./img/details-4.jpeg";
+import img1 from "./img/room-1.jpeg";
+import img2 from "./img/room-2.jpeg";
+import img3 from "./img/room-3.jpeg";
+import img4 from "./img/room-4.jpeg";
+import img5 from "./img/room-5.jpeg";
+import img6 from "./img/room-6.jpeg";
+import img7 from "./img/room-7.jpeg";
+import img8 from "./img/room-8.jpeg";
+import img9 from "./img/room-9.jpeg";
+import img10 from "./img/room-10.jpeg";
+import img11 from "./img/room-11.jpeg";
+import img12 from "./img/room-12.jpeg";
 
 export default [
   {
