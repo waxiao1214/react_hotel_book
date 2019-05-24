4efe44f9a5bce537ca1a4b5a3a52f4c162d5c57e
highqcoder <wxiao1214@gmail.com>
2019-05-24 18:48:02 +0200

    Exclude hero image from detail images

--- src/components/SingleRoom.js
+++ src/components/SingleRoom.js
@@ -37,9 +37,10 @@ export default class SingleRoom extends Component {
             pets,
             images,
           } = room;
+          const [mainImg, ...defaultImg] = images;
           return (
             <React.Fragment>
-              <StyledHero img={images[0] || this.state.defaultBg}>
+              <StyledHero img={mainImg || this.state.defaultBg}>
                 <Banner title={`${name} room`}>
                   <Link to='/rooms' className='btn-primary'>
                     back to rooms
@@ -48,7 +49,7 @@ export default class SingleRoom extends Component {
               </StyledHero>
               <section className='single-room'>
                 <div className='single-room-images'>
-                  {images.map((item, index) => (
+                  {defaultImg.map((item, index) => (
                     <img src={item} alt={name} key={index} />
                   ))}
                 </div>
