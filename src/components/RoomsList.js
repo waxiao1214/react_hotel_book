64152f4f219a22db17df9cb5e6cbd167fd9ffd32
highqcoder <wxiao1214@gmail.com>
2019-06-04 20:57:48 +0200

    Create RoomsList component

--- src/components/RoomsList.js
+++ src/components/RoomsList.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import Room from './Room';
 
-export default function RoomsList() {
-  return (
-    <div>
-      <h3>Hello from RoomsList</h3>
+export default function RoomsList({ rooms }) {
+  return rooms.length === 0 ? (
+    <div className="empty-search">
+      <h3>no rooms matched your search parameters</h3>
     </div>
+  ) : (
+    <section className="rooms-list">
+      <div className="rooms-list-center">
+        {rooms.map(item => (
+          <Room key={item.id} room={item} />
+        ))}
+      </div>
+    </section>
   );
 }
