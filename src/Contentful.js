b4415862fb56b78a802cf74a53b181204fc74012
princez1214 <wxiao1214@gmail.com>
2020-08-08 06:59:02 +0800

    Revert "updated reademe"
    This reverts commit 98e1c21d71019a677f388ddd01ca1e8eb991862b.

--- src/Contentful.js
+++ src/Contentful.js
@@ -1,6 +1,6 @@
 import { createClient } from 'contentful';
 
 export default createClient({
-  space: 'qmjjuf4l2x9s',
-  accessToken: '9mIlqvH7U9t3eyNQTAbvk4XPaNLgqM-AZLmLHpaPK1w',
+  space: process.env.REACT_APP_API_SPACE,
+  accessToken: process.env.REACT_APP_ACCESS_TOKEN,
 });
